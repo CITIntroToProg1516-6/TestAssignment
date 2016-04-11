d93dc2b225b9e642769e9111e944e5f3e1c46bd8
hcps-barronpm <hcps-barronpm@180-18BARRONPM.hcps.internal>
2016-04-11 14:51:39 -0400

    Made it 100% Neville approved

--- src/main/java/cit/programming/testassignment/TestClass.java
+++ src/main/java/cit/programming/testassignment/TestClass.java
@@ -1,18 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cit.programming.testassignment;
 
-/**
- *
- * @author chris
- */
 public class TestClass {
     public static void main(String[] args){
-        System.out.println("helloworld");
-        //comment
+        do { break; } while(true);
         
+        int i = 7;
+        int[] j = new int[++i];
+        
+        for (int k = 0; k < j.length; k++) {
+            j[k] = j[i++] + j[j[--i]];
+        }
+        
+        System.out.println(j[i++] + j[j[--i]]);
     }
 }
